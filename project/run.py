b6a65b5045d7f051268030f228f8ac7566653bd4
khalil dahmeni <khalilthenew@gmail.com>
2026-03-26 18:08:47 +0100

    refactoring

--- project/run.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    # Lance le serveur web
-    app.run(debug=True)
--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from app.app import app
+from app import create_app
 
-if __name__ == "__main__":
+app = create_app()
+
+if __name__ == '__main__':
+    # Lance le serveur web
     app.run(debug=True)
